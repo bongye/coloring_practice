46efae146eafe0c8bfbfcb5868245ca87827d553
bongye <bongye@wafflestudio.com>
2011-08-12 11:33:02 +0900

    logo revised. some view page added.

--- app/helpers/drawings_helper.rb
+++ app/helpers/drawings_helper.rb
@@ -1,2 +1,14 @@
+#coding: utf-8
 module DrawingsHelper
+  def is_drawer?(drawing, user)
+    !user.nil? && drawing.user_id == user.id
+  end
+
+  def drawing_delete_link(drawing)
+    link_to('삭제', "pictures/#{drawing.picture.id}/drawings/#{drawing.id}", :method => :delete, :confirm => '정말 삭제하시겠습니까?')
+  end
+
+  def drawing_edit_link(drawing)
+    link_to('수정', edit_picture_drawing_path(drawing.picture, drawing))
+  end
 end
--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -4,7 +4,7 @@ module PicturesHelper
     !user.nil? && picture.user_id == user.id
   end
   
-  def picture_delete_link(picture, user)
+  def picture_delete_link(picture)
     link_to('삭제', picture_path(picture), :method => :delete, :confirm => '정말 삭제하시겠습니까?');
   end
 end
